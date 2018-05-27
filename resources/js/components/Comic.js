6e93f27bb99d43f1280bce71b3ada34359a93d6b
Caio Navarro <caionavarro.n@hotmail.com>
2018-05-27 14:26:03 -0300

    Fix error of comic without image

--- resources/js/components/Comic.js
+++ resources/js/components/Comic.js
@@ -6,13 +6,15 @@ class Comic extends React.Component {
   }
   render () {
     const image = this.props.comic.images[0]
-    const imagePath = image.path
-    const imageExtension = image.extension
+    const imagePath = image ? image.path : false
+    const imageExtension = image ? image.extension : false
 
     return (
       <div>
         <h3>{this.props.comic.title}</h3>
-        <img src={`${imagePath}/${this.state.imageFormat}.${imageExtension}`} />
+        { imagePath && imageExtension &&
+          <img src={`${imagePath}/${this.state.imageFormat}.${imageExtension}`} />
+        }
       </div>
     )
   }
