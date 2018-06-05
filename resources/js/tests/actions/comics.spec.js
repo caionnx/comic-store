441a17b264b0e8c6920097e9f172543fd2857079
Caio Navarro <caionavarro.n@hotmail.com>
2018-06-04 23:26:35 -0300

    Add tests for Comics Actions

---
+++ resources/js/tests/actions/comics.spec.js
@@ -0,0 +1,64 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import * as actions from '../../actions/comics'
+import { comics, request } from '../fixtures/comics'
+
+const mockRequestData = request.data
+const createMockStore = configureMockStore([thunk])
+jest.mock('../../api/marvelApi', () => ({
+  getComics: () => new Promise(resolve => {
+    resolve(mockRequestData)
+  })
+}))
+
+test('Should setup set comics action object', () => {
+  const action = actions.setComics(comics)
+  expect(action).toEqual({
+    type: 'SET_COMICS',
+    comics
+  })
+})
+
+test('Should setup add comics action object', () => {
+  const action = actions.addComics(comics)
+  expect(action).toEqual({
+    type: 'ADD_COMICS',
+    comics
+  })
+})
+
+test('Should start set comics from API', (done) => {
+  const store = createMockStore([])
+
+  store.dispatch(actions.startSetComics({ dateDescriptor: 'thisWeek' })).then((dataFromRequest) => {
+    const actions = store.getActions()
+
+    expect(actions[0]).toEqual({
+      type: 'SET_COMICS',
+      comics
+    })
+    expect(dataFromRequest.offset).toBe(request.data.offset)
+    expect(dataFromRequest.count).toBe(request.data.count)
+    expect(dataFromRequest.total).toBe(request.data.total)
+
+    done()
+  })
+})
+
+test('Should start add comics from API', (done) => {
+  const store = createMockStore([])
+
+  store.dispatch(actions.startAddComics({ dateDescriptor: 'thisWeek' })).then((dataFromRequest) => {
+    const actions = store.getActions()
+
+    expect(actions[0]).toEqual({
+      type: 'ADD_COMICS',
+      comics
+    })
+    expect(dataFromRequest.offset).toBe(request.data.offset)
+    expect(dataFromRequest.count).toBe(request.data.count)
+    expect(dataFromRequest.total).toBe(request.data.total)
+
+    done()
+  })
+})
