3e73fa15e088959a7f2d025637d85dc70f01cf0e
Caio Navarro <caionavarro.n@hotmail.com>
2018-06-17 22:15:50 -0300

    Add new reducer case to apply price with discount

--- resources/js/reducers/cart.js
+++ resources/js/reducers/cart.js
@@ -4,6 +4,12 @@ export default (state = [], action) => {
       return [...state, action.comic]
     case 'REMOVE_COMIC':
       return state.filter(c => c.id !== action.id)
+    case 'EDIT_COMIC':
+      return state.map((comic) =>
+        comic.id === action.id
+          ? { ...comic, ...action.updates }
+          : comic
+      )
     default:
       return state
   }
