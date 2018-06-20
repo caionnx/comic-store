9c06dc4d42ebfa6246f4975e6ad259da857c2f35
Caio Navarro <caionavarro.n@hotmail.com>
2018-06-19 23:21:09 -0300

    Implements logic for re-apply discounts when didMount component

--- resources/js/components/ComicCartDiscounts.js
+++ resources/js/components/ComicCartDiscounts.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { editComicOfCart } from '../actions/cart'
+import { editComicOfCart, addDiscountObjectToCart } from '../actions/cart'
 
 class ComicCartDiscounts extends React.Component {
-  discounts = {
-    applied: [],
-    list: [
-      {
-        test: /[0-9]\w+([A-Z]{2})/,
-        value: 10,
-        type: 'normal',
-        rule: (comic) => !comic.rareIssue
-      },
-      {
-        test: /([A-Z]{2})[0-9]{4}[a-z]/,
-        value: 5,
-        type: 'rare',
-        rule: (comic) => comic.rareIssue
-      }
-    ]
-  }
+  discountsTester = [
+    {
+      test: /[0-9]\w+([A-Z]{2})/,
+      value: 10,
+      type: 'normal',
+      rule: (comic) => !comic.rareIssue
+    },
+    {
+      test: /([A-Z]{2})[0-9]{4}[a-z]/,
+      value: 5,
+      type: 'rare',
+      rule: (comic) => comic.rareIssue
+    }
+  ]
 
   getTypeOfDiscount = (code) => {
     let toReturn = null
-    for (const discount of this.discounts.list) {
+    for (const discount of this.discountsTester) {
       const { test, type, value, rule } = discount
       const match = code.match(test)
       if (match) {
@@ -40,9 +37,7 @@ class ComicCartDiscounts extends React.Component {
     return price || 0
   }
 
-  handleDiscount = (discount) => {
-    const comicsToApply = this.props.cart.filter(discount.rule)
-
+  handleDiscount = (discount, comicsToApply) => {
     comicsToApply.forEach(comic => {
       const price = this.getPrice(comic)
       const priceWithDiscount = price - ((price * discount.value) / 100)
@@ -51,16 +46,28 @@ class ComicCartDiscounts extends React.Component {
     })
   }
 
+  componentDidMount () {
+    const { cart, discountsList } = this.props
+
+    discountsList.forEach(discount => {
+      const comicsToApply = cart.filter(item => !item.priceWithDiscount).filter(discount.rule)
+      this.handleDiscount(discount, comicsToApply)
+    })
+  }
+
   onApplyDiscount = (ev) => {
     ev.preventDefault()
-    const { applied: appliedDiscounts } = this.discounts
+    let comicsToApply
+    const { cart, discountsList, addObjToDiscountsList } = this.props
     const code = ev.target.querySelector('#discount-input').value
     const typeOfDiscount = this.getTypeOfDiscount(code)
-    if (!appliedDiscounts.includes(code) && typeOfDiscount) {
-      this.handleDiscount(typeOfDiscount)
-      appliedDiscounts.push(code)
+    if (!discountsList.includes(code) && typeOfDiscount) {
+      comicsToApply = cart.filter(typeOfDiscount.rule)
+      this.handleDiscount(typeOfDiscount, comicsToApply)
+      addObjToDiscountsList({...typeOfDiscount, code})
     }
   }
+
   render () {
     return (
       <form onSubmit={this.onApplyDiscount} className='l-input-group'>
@@ -76,11 +83,13 @@ class ComicCartDiscounts extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  cart: state.cart
+  cart: state.cart.items,
+  discountsList: state.cart.discounts
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  updateComic: (id, updates) => dispatch(editComicOfCart(id, updates))
+  updateComic: (id, updates) => dispatch(editComicOfCart(id, updates)),
+  addObjToDiscountsList: (discount) => dispatch(addDiscountObjectToCart(discount))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ComicCartDiscounts)
