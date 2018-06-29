891532a59545875f8e3458415e48a19de1ac44d1
Caio Navarro <caionavarro.n@hotmail.com>
2018-06-28 22:04:47 -0300

    Fix jest test for cart reducer

--- resources/js/tests/reducers/cart.spec.js
+++ resources/js/tests/reducers/cart.spec.js
@@ -1,24 +1,24 @@
-import reducer from '../../reducers/cart'
+import reducer, { cartReducerDefaultState } from '../../reducers/cart'
 import * as actions from '../../actions/cart'
 import { comics } from '../fixtures/comics'
 
 test('Should set default state', () => {
   const state = reducer(undefined, { type: '@@INIT' })
-  expect(state).toEqual([])
+  expect(state).toEqual(cartReducerDefaultState)
 })
 
 test('Should add comic to cart', () => {
   const oneComic = comics[0]
   const action = actions.addComicToCart(oneComic)
-  const state = reducer([], action)
+  const state = reducer(cartReducerDefaultState, action)
 
-  expect(state).toEqual([oneComic])
+  expect(state.items).toEqual([oneComic])
 })
 
 test('Should remove comic from cart', () => {
   const oneComic = comics[0]
   const action = actions.removeComicFromCart(oneComic.id)
-  const state = reducer([], action)
+  const state = reducer(cartReducerDefaultState, action)
 
-  expect(state).toEqual([])
+  expect(state.items).toEqual([])
 })
