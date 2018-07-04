54d9ea07a5a465c3702f4abe45965277a63e36f7
Caio Nunes <caionavarro.n@hotmail.com>
2018-07-04 18:01:00 -0300

    Add polyfill for Jest: Object.entries

--- resources/js/tests/setupTests.js
+++ resources/js/tests/setupTests.js
@@ -7,3 +7,15 @@ Enzyme.configure({
 })
 
 DotEnv.config({ path: '.env.test' })
+
+// Polyfill
+// https://github.com/tc39/proposal-object-values-entries/blob/master/polyfill.js
+if (!Object.entries) {
+  const reduce = Function.bind.call(Function.call, Array.prototype.reduce)
+  const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable)
+  const concat = Function.bind.call(Function.call, Array.prototype.concat)
+  const keys = Reflect.ownKeys
+  Object.entries = function entries (O) {
+    return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+  }
+}
